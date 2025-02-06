379256c63c3f3b87ac82835736ed2d45b84dd3b6
Tanmay0Bhosale <113248930+Tanmay0Bhosale@users.noreply.github.com>
2025-02-06 09:12:50 +0530

    latest update

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 function App() {
   
+console.log(import.meta.env.VITE_APP_APPWRITE_URL);
 
   return (
     <>
---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,5 @@
+import conf from '../conf/conf.js';
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService{}
+export default AuthService;
